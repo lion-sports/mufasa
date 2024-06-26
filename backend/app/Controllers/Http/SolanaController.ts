308a789ee49142549c5b505162cc3e2bfcbdcb0e
filippopassalacqua <filippo.passalacqua.13@gmail.com>
2024-06-26 19:56:40 +0200

    86bz461pn

--- backend/app/Controllers/Http/SolanaController.ts
+++ backend/app/Controllers/Http/SolanaController.ts
@@ -8,19 +8,21 @@ export default class SolanaController {
   public async getReward(){
 
 
+    //use a factory to random generate user
+
     const manager =  new UsersManager()
     let createdUser = await manager.create(
-      {
-        data:{
-      firstname: "platform",
-      email: "admin@platform.it",
-      lastname: "blockchain",
-      password: "qQxSHimmk6F9MX"
-    }})
+        {
+          data:{
+          firstname: "platform", //
+          email: "admin@platform.it", //random generate username
+          lastname: "blockchain", 
+          password: "qQxSHimmk6F9MX" // same as username
+      }})
 
 
     //transfer from Token accaount to wallet from user
-    
+
 
   }
 }
