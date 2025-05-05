d6ac8f01b3e44c52407b3edd684567cd826030de
Giacomo <giacomo.folli@studenti.unipr.it>
2025-05-05 11:45:45 +0200

    LION-131  - fix convocations page

--- frontend/src/routes/(main)/teams/[teamId]/events/[eventId]/convocations/+page.svelte
+++ frontend/src/routes/(main)/teams/[teamId]/events/[eventId]/convocations/+page.svelte
@@ -6,13 +6,13 @@
 	import event from '$lib/stores/events/eventShow'
 	import team from '$lib/stores/teams/teamsShow'
 	import { Icon } from '@likable-hair/svelte'
-  import type { PageData } from './$types'
+	import type { PageData } from './$types'
 
 	interface Props {
-		data: PageData;
+		data: PageData
 	}
 
-	let { data }: Props = $props();
+	let { data }: Props = $props()
 
 	function handleConfirmOrDeny(e: CustomEvent<{ convocation: Convocation }>) {
 		if (!!$event) {
@@ -24,9 +24,6 @@
 	}
 
 	let convocationDialogOpen: boolean = $state(false)
-	function openDialog() {
-		convocationDialogOpen = true
-	}
 
 	function handleConvocate(
 		e: CustomEvent<{
@@ -50,24 +47,26 @@
 		}
 	}
 
-	let teammatesToConvocate = $derived(!!$team
-		? $team.teammates.filter((tm) => {
-				return (
-					!!$event &&
-					!$event.convocations.map((c) => c.teammateId).includes(tm.id) &&
-					(tm.group === undefined || tm.group === null || tm.group?.convocable)
-				)
-		})
-		: [])
+	let teammatesToConvocate = $derived(
+		!!$team
+			? $team.teammates.filter((tm) => {
+					return (
+						!!$event &&
+						!$event.convocations.map((c) => c.teammateId).includes(tm.id) &&
+						(tm.group === undefined || tm.group === null || tm.group?.convocable)
+					)
+				})
+			: []
+	)
 </script>
 
 <div style:margin-top="20px">
 	<ConvocationList
 		convocations={$event?.convocations}
 		team={$team}
-    canConfirm={data.groupedPermissions.convocation.confirm}
-    canDeny={data.groupedPermissions.convocation.deny}
-    canConvocate={data.groupedPermissions.event.convocate}
+		canConfirm={data.groupedPermissions.convocation.confirm}
+		canDeny={data.groupedPermissions.convocation.deny}
+		canConvocate={data.groupedPermissions.event.convocate}
 		on:confirm={handleConfirmOrDeny}
 		on:deny={handleConfirmOrDeny}
 		on:unConvocate={handleUnConvocate}
@@ -81,7 +80,7 @@
 		style:display="flex"
 		style:justify-content="center"
 	>
-		<Icon name="mdi-plus" click on:click={openDialog} />
+		<Icon name="mdi-plus" onclick={() => (convocationDialogOpen = true)} />
 	</div>
 {/if}
 
