deb65ef19cccaef69e9500d72cc6684fbd3244d7
Gabriele Garlaschelli <gabriele.garlaschelli@likablehair.it>
2025-05-05 10:35:03 +0200

    LION-131 Refactor StandardChip component to support custom onclick and onclose event handlers

--- frontend/src/lib/components/common/StandardChip.svelte
+++ frontend/src/lib/components/common/StandardChip.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-
 	import { Chip } from '@likable-hair/svelte'
+	import type { ComponentProps } from 'svelte'
+
 	interface Props {
 		close?: boolean;
 		closeIcon?: string;
 		text?: string | undefined;
 		fontWeight?: number | undefined;
 		children?: import('svelte').Snippet;
+    onclick?: ComponentProps<typeof Chip>['onclick']
+    onclose?: ComponentProps<typeof Chip>['onclose']
 	}
 
 	let {
@@ -14,11 +17,13 @@
 		closeIcon = 'mdi-close',
 		text = undefined,
 		fontWeight = undefined,
-		children
+		children,
+    onclick,
+    onclose
 	}: Props = $props();
 </script>
 
-<Chip label {close} {closeIcon} on:click on:close>
+<Chip label {close} {closeIcon} {onclick} {onclose}>
 	<div
 		style:min-height="30px"
 		style:display="flex"
