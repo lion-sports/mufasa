0f4b47b972db8b423f6c47fd5c684e10c5dfb3af
Gabriele Garlaschelli <gabriele.garlaschelli@likablehair.it>
2025-05-05 10:45:43 +0200

    LION-131 Refactor StandardSwitch component to support onchange prop and improve event handling

--- frontend/src/lib/components/common/StandardSwitch.svelte
+++ frontend/src/lib/components/common/StandardSwitch.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import { Switch } from '@likable-hair/svelte'
+	import type { ComponentProps } from 'svelte'
+
 	interface Props {
 		value: boolean;
+    onchange?: ComponentProps<typeof Switch>['onchange']
 	}
 
-	let { value = $bindable() }: Props = $props();
+	let { value = $bindable(), onchange }: Props = $props();
 </script>
 
-<Switch bind:value on:change />
+<Switch bind:value {onchange} />
