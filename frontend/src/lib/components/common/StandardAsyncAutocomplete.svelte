69a0d917960fa3361c6222729f708903d69c8a81
Giacomo <giacomo.folli@studenti.unipr.it>
2025-05-05 10:38:42 +0200

    LION-131 - standard async autocomplete

--- frontend/src/lib/components/common/StandardAsyncAutocomplete.svelte
+++ frontend/src/lib/components/common/StandardAsyncAutocomplete.svelte
@@ -3,34 +3,36 @@
 	import type { ComponentProps } from 'svelte'
 
 	interface Props {
-		values?: ComponentProps<AsyncAutocomplete>['values'];
-		items?: ComponentProps<AsyncAutocomplete>['items'];
-		searcher: ComponentProps<AsyncAutocomplete>['searcher'];
-		placeholder?: string;
-		multiple?: boolean;
-		mobileDrawer?: boolean;
-		disabled?: boolean;
+		values?: ComponentProps<typeof AsyncAutocomplete>['values']
+		items?: ComponentProps<typeof AsyncAutocomplete>['items']
+		searcher: ComponentProps<typeof AsyncAutocomplete>['searcher']
+		onchange?: ComponentProps<typeof AsyncAutocomplete>['onchange']
+		placeholder?: string
+		multiple?: boolean
+		mobileDrawer?: boolean
+		disabled?: boolean
 	}
 
 	let {
-		values = $bindable([]),
+		values = $bindable(),
 		items = $bindable([]),
 		searcher = $bindable(),
 		placeholder = $bindable('Keep writing ...'),
 		multiple = $bindable(false),
 		mobileDrawer = $bindable(false),
-		disabled = $bindable(false)
-	}: Props = $props();
+		disabled = $bindable(false),
+		onchange
+	}: Props = $props()
 </script>
 
 <AsyncAutocomplete
-	bind:values
-  bind:items
-	bind:searcher
-	bind:mobileDrawer
-	bind:multiple
-	bind:placeholder
-  bind:disabled
-	on:change
+	{values}
+	{multiple}
+	{disabled}
+	{items}
+	{placeholder}
+	{mobileDrawer}
+	{searcher}
+	{onchange}
 	--autocomplete-min-height="44px"
 />
