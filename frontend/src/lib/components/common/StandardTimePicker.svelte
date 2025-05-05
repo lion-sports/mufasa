0cbd416140d212ecc7125400cc05a005a7eafa7c
Gabriele Garlaschelli <gabriele.garlaschelli@likablehair.it>
2025-05-05 10:37:10 +0200

    LION-131 Update StandardTimePicker to make 'name' prop optional and support custom oninput event handling

--- frontend/src/lib/components/common/StandardTimePicker.svelte
+++ frontend/src/lib/components/common/StandardTimePicker.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import StandardTextfield from './StandardTextfield.svelte'
+  import type { ComponentProps } from 'svelte';
 
 	interface Props {
 		value?: string;
-		name: string;
+		name?: string;
+    oninput?: ComponentProps<typeof StandardTextfield>['oninput']
 	}
 
-	let { value = $bindable(''), name }: Props = $props();
+	let { value = $bindable(''), name, oninput }: Props = $props();
 </script>
 
 <StandardTextfield
@@ -14,5 +16,6 @@
 	appendInnerIcon="mdi-clock"
 	type="time"
 	--simple-textfield-width="fit-content"
-	on:input
+  {name}
+	{oninput}
 />
